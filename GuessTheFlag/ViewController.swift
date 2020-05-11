368470391ceba184f30319121f16172773722dbd
Jack Knight <jkknight9@gmail.com>
2020-05-11 12:02:47 -0600

    Added to the UI controller the ability to detect if the user guesses the wrong flag

--- GuessTheFlag/ViewController.swift
+++ GuessTheFlag/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     var countries = [String]()
     var score = 0
     var correctAnswer = 0
+    var answerSelected = 0
     var questions = 0
     
     override func viewDidLoad() {
@@ -36,32 +37,33 @@ class ViewController: UIViewController {
     func askQuestion(action: UIAlertAction! = nil) {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+//        answerSelected = Int.random(in: 0...2)
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
-        title = "\(countries[correctAnswer].uppercased())                 Score: \(score)"
+        title = "\(countries[correctAnswer].uppercased())                           Score: \(score)"
     }
     
     
     @IBAction func buttonTapped(_ sender: UIButton) {
-        var title: String
+        var title: String = "Wrong!"
         if questions < 9 {
-            print(questions)
             if sender.tag == correctAnswer {
-                title = "Correct."
+                title = "Correct That is the \(countries[correctAnswer].uppercased()) flag!."
                 questions += 1
                 score += 1
-            } else {
-                title = "Wrong. "
+            } else if sender.tag != correctAnswer {
+                answerSelected = sender.tag
+                title = "Wrong. That is the \(countries[answerSelected].uppercased()) flag!"
                 questions += 1
                 score -= 1
             }
-            let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
-            present(ac, animated: true)
-        }
-        else {
-            let AC = UIAlertController(title: "Great Job", message: "Your fianl score is \(score)", preferredStyle: .alert)
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
+        } else {
+            score += 1
+            let AC = UIAlertController(title: "Great Job", message: "Your final score is \(score)", preferredStyle: .alert)
             AC.addAction(UIAlertAction(title: "Play Again", style: .default, handler: askQuestion))
             present(AC, animated: true)
             score = 0
